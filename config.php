648efff443109c0a5699a14b3f37d9451b99071e
Abel <abel.na.lima@gmail.com>
2023-08-02 22:34:05 -0300

    Criando o config.php e declarando variaveis globais

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php 
+    //VARIAVEIS GLOBAIS
+    $email = "abel.na.lima@gmail.com";
+    $telefone = "(81) 3626-0000";
+    $whatsapp = "(81) 9 9999-9999";
+    $whatsapp_link = "5581999999999"
+?>
