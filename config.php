852666f4d61eb6bd6f1d82101826666e7c192464
Abel <abel.na.lima@gmail.com>
2023-08-06 12:13:13 -0300

    change: Alterando o menu mobile

--- config.php
+++ config.php
@@ -3,5 +3,7 @@
     $email = "abel.na.lima@gmail.com";
     $telefone = "(81) 3626-0000";
     $whatsapp = "(81) 9 9999-9999";
-    $whatsapp_link = "5581999999999"
+    $whatsapp_link = "5581999999999";
+    $nome_loja = "Boutique Freitas";
+    $texto_destaque = "Frete grátis para compras acimas de R$300,00"
 ?>
