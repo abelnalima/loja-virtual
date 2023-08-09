b4d9aee22b70b042a5c9a12cfa1b3065b0543501
Abel <abel.na.lima@gmail.com>
2023-08-09 16:34:36 -0300

    change: Alterando as tags do <head>

--- cabecalho.php
+++ cabecalho.php
@@ -7,11 +7,14 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Ogani Template">
-    <meta name="keywords" content="Ogani, unica, creative, html">
+    <meta name="description" content="Vendas de roupas masculina e feminina">
+    <meta name="keywords" content="botas masculinas, roupas femininas">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $nome_loja ?></title>
+
+    <!-- FAV Icon -->
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;900&display=swap" rel="stylesheet">
@@ -25,6 +28,8 @@
     <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
     <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
     <link rel="stylesheet" href="css/style.css" type="text/css">
+
+
 </head>
 
 <body>
